b14300f5f8a206ea8be2d345f4986f60d6068f27
Anilluh <asingh9_be20@thapar.edu>
2022-07-19 00:11:51 +0530

    Connected wit MongoDB and added CRUD

--- backend/routes/controllers/getGoal.js
+++ backend/routes/controllers/getGoal.js
@@ -1,8 +1,10 @@
+const { Console } = require('console');
 const asyncHandler=require('express-async-handler');
-
+const GOAL=require('../../models/goalModel')
 // Method GET /api.goals
 const getGoals=asyncHandler( async (req,res)=>{
-    res.send('getGoals');
+    const goals=await GOAL.find();
+    res.status(200).json(goals);
 })
 
 // Method POST /api.goals
@@ -13,18 +15,41 @@ const setGoals= asyncHandler( async (req,res)=>{
     throw new Error('Please add a text field');
     }
     else{
-        res.status(200).json({messgae:`Message recieved ${req.body.text}`});
+        const goals=await GOAL.create({text:req.body.text,})
+        res.status(200).json({messgae:`Message recieved ${goals}`})
     }
 })
 
 // Method PUT /api.goals/:id
 const updateGoals=asyncHandler( async (req,res)=>{
-    res.send(`Update Goal of id ${req.params.id}`);
+   const goals=await GOAL.findById(req.params.id)
+   if(!goals){
+    res.status(400);
+    throw new Error("id not found");
+   }
+   else{
+    console.log('id found');
+    const updatedGoal=await GOAL.findByIdAndUpdate(req.params.id,req.body,{new:true});
+    // console.log(updateGoals);
+    res.status(200).json(updateGoals)
+   }
 })
 
 // Method DELETE /api.goals/:id
 const deleteGoals=asyncHandler( async (req,res)=>{
-    res.send(`Delete Goal of id ${req.params.id}`);
+    const goals=await GOAL.findById(req.params.id)
+   if(!goals){
+    res.status(400);
+    throw new Error("id not found");
+   }
+   else{
+    // console.log('id found');
+    goals.remove();
+    // const deletedGoal=await GOAL.remove(id: req.params.id);
+    // console.log(updateGoals);
+    // res.status(200).json(deleteGoals)
+    res.send('deleted')
+   }
 })
 
 module.exports={
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express=require('express');
 const app=express();
+const colours=require('colours')
 const dotenv=require('dotenv').config();
+const connectDB=require('./config/db')
 const port = process.env.PORT || 5000;
+
+connectDB()
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
